28bdfc78c6ce5c6a64a65aafbed63d2cd2b896ab
ticoniq_ <okolietochi@gmail>
2023-08-23 13:39:45 +0100

    Updated and display the code

--- decode.rb
+++ decode.rb
@@ -7,14 +7,26 @@ MORSE_CODE = {
   '--..' => 'Z'
 }.freeze
 
-def decode_char(char) 
-  MORSE_CODE[char] 
+def decode_char(char)
+  MORSE_CODE[char]
 end
 
 def decode_word(word)
   word.split
     .map do |char|
-      decode_char char
+    decode_char char
   end
-  .join('')
+    .join('')
 end
+
+def decode(msg)
+  msg.split('   ')
+    .map do |word|
+    decode_word word
+  end
+    .join(' ')
+end
+
+puts(decode_char('.-'))
+puts(decode_word('-- -.--'))
+puts(decode('-- -.--   -. .- -- .'))
